5f107c9d7760004877b391767509417a96bcc50f
Thevic16 <20171408@ce.pucmm.edu.do>
2021-03-25 14:01:27 -0400

    getHerokuAssignePort

--- Práctica #2 - CRUD/src/main/java/edu/pucmm/eict/Main.java
+++ Práctica #2 - CRUD/src/main/java/edu/pucmm/eict/Main.java
@@ -16,16 +16,29 @@ public class Main {
         -Lista de usuarios visible por todo el sistema (Apartado 2)
         -Lista de productos visible por todo el sistema (Apartado 3)
         */
-
-
         //Creando la instancia del servidor.
-        Javalin app = Javalin.create().start(7000);
+        //Creando la instancia del servidor.
+        Javalin app = Javalin.create(config ->{
+            config.addStaticFiles("/publico"); //desde la carpeta de resources
+            config.enableCorsForAllOrigins();
+        });
         app.config.addStaticFiles("/publico");
+        app.start(getHerokuAssignedPort());
 
 
         new Ruta(app,administracion).ejecutarRutas();
     }
 
-
+    /**
+     * Metodo para indicar el puerto en Heroku
+     * @return
+     */
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 7000; //Retorna el puerto por defecto en caso de no estar en Heroku.
+    }
 }
 
